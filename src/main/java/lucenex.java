a0b119f989984a4a6b2bdf63f69538bd49910b1b
FrankDena <116834956+FrankDena@users.noreply.github.com>
2024-11-05 10:59:31 +0100

    Primo test su slide lezione
    Fatto un primo esempio di creazione indice e query che ritorna tutti i documenti

--- src/main/java/lucenex.java
+++ src/main/java/lucenex.java
@@ -1,11 +1,105 @@
 import java.util.Scanner;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.CharArraySet;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.core.WhitespaceAnalyzer;
+import org.apache.lucene.analysis.miscellaneous.PerFieldAnalyzerWrapper;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.codecs.Codec;
+import org.apache.lucene.document.*;
+import org.apache.lucene.index.*;
+import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.*;
+import org.apache.lucene.search.similarities.BM25Similarity;
+import org.apache.lucene.search.similarities.ClassicSimilarity;
+import org.apache.lucene.search.similarities.LMJelinekMercerSimilarity;
+import org.apache.lucene.search.similarities.Similarity;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.codecs.simpletext.SimpleTextCodec;
+
+
 public class lucenex {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // Prints "Hello, World" in the terminal window.
-        Scanner sc= new Scanner(System.in); //System.in is a standard input stream
-        System.out.print("Enter a string: ");
-        String str= sc.nextLine();              //reads string
-        System.out.print("You have entered: "+str);
+        Path projectDir = Paths.get("").toAbsolutePath();
+        Path path = projectDir.resolve("lucene-idx");
+        matchAllDocs(path);
+
+    }
+
+    private static void matchAllDocs(Path path) throws IOException {
+        Query query = new MatchAllDocsQuery();
+
+        try (Directory directory = FSDirectory.open(path)) {
+            indexDocs(directory, new SimpleTextCodec());
+            try (IndexReader reader = DirectoryReader.open(directory)) {
+                IndexSearcher searcher = new IndexSearcher(reader);
+                runQuery(searcher, query);
+                //reader.close();
+            } finally {
+                directory.close();
+            }
+
+        }
+    }
+
+    private static void runQuery(IndexSearcher searcher, Query query) throws IOException {
+        StandardAnalyzer analyzer = new StandardAnalyzer();
+        TopDocs hits = searcher.search(query, 10);
+
+        // 6. Gestisci i risultati
+        System.out.println("Risultati trovati: " + hits.totalHits.value);
+
+        for (ScoreDoc scoreDoc : hits.scoreDocs) {
+            Document document = searcher.doc(scoreDoc.doc);
+            System.out.println("Documento trovato: " + document.get("contenuto"));
+        }
+
+
+    }
+
+    private static void indexDocs(Directory directory, Codec codec) throws IOException {
+        Analyzer defaultAnalyzer = new StandardAnalyzer();
+        CharArraySet stopWords = new CharArraySet(Arrays.asList("in", "dei", "di"), true);
+        Map<String, Analyzer> perFieldAnalyzers = new HashMap<>();
+        perFieldAnalyzers.put("contenuto", new StandardAnalyzer(stopWords));
+        perFieldAnalyzers.put("titolo", new WhitespaceAnalyzer());
+
+        Analyzer analyzer = new PerFieldAnalyzerWrapper(defaultAnalyzer, perFieldAnalyzers);
+        IndexWriterConfig config = new IndexWriterConfig(analyzer);
+        if (codec != null) {
+            config.setCodec(codec);
+        }
+        IndexWriter writer = new IndexWriter(directory, config);
+        writer.deleteAll();
+
+        Document doc1 = new Document();
+        doc1.add(new TextField("titolo", "Come diventare un ingegnere dei dati, Data Engineer?", Field.Store.YES));
+        doc1.add(new TextField("contenuto", "Sembra che oggigiorno tutti vogliano diventare un Data Scientist  ...", Field.Store.YES));
+        doc1.add(new StringField("data", "12 ottobre 2016", Field.Store.YES));
+
+        Document doc2 = new Document();
+        doc2.add(new TextField("titolo", "Curriculum Ingegneria dei Dati - Sezione di Informatica e Automazione", Field.Store.YES));
+        doc2.add(new TextField("contenuto", "Curriculum. Ingegneria dei Dati. Laurea Magistrale in Ingegneria Informatica ...", Field.Store.YES));
+
+        writer.addDocument(doc1);
+        writer.addDocument(doc2);
+
+        writer.commit();
+        writer.close();
     }
 }
